8a7b38a0db1d18a4584b4170fccfc8e8e2309475
saikumarnunsk <saikumarnunnagoppula@gmail.com>
2025-05-26 10:28:59 +0530

    playing with rotes

--- src/app.js
+++ src/app.js
@@ -7,12 +7,24 @@ const app = express() // created a server
 // Router, request handler
 // order of the writing the code is an importent matter
 
-app.get("/user", (req, res)=>{
+app.get("/user/:id/:useName", (req, res)=>{
+    // dynamic routes
+    console.log(req.params)
     res.send({
         firstName: "saikumar", 
         age: 26
     })
 })
+
+app.get("/user", (req, res)=>{
+    // accessing query parsms
+    console.log(req.query)
+    res.send({
+        firstName: "saikumar", 
+        age: 26
+    })
+})
+
 
 app.post("/user", (req, res)=>{
     // write some code data in DB
@@ -32,4 +44,3 @@ app.use("/test",(req,res)=>{
 app.listen(3000, ()=>{
     console.log('server is successfully listaning on port 3000')
 })
-
