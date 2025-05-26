3b94101be2d5b9230a79920abaa2245fbdaf97c2
saikumarnunsk <saikumarnunnagoppula@gmail.com>
2025-05-26 10:10:09 +0530

    ecploring get, post, delete calls

--- src/app.js
+++ src/app.js
@@ -5,10 +5,29 @@ const express = require('express')
 const app = express() // created a server
 
 // Router, request handler
+// order of the writing the code is an importent matter
 
-app.use("/test",(req,res)=>{
-    res.send("Hello I am listaningds tested!!!!")
+app.get("/user", (req, res)=>{
+    res.send({
+        firstName: "saikumar", 
+        age: 26
+    })
 })
+
+app.post("/user", (req, res)=>{
+    // write some code data in DB
+    res.send("Data stored successfully")
+})
+
+app.delete("/user", (req, res)=>{
+    // write some code data in DB
+    res.send("Data deleted successfully")
+})
+// if you use "use" mathod it will match with all the http methods
+app.use("/test",(req,res)=>{
+    res.send("this is test rote")
+})
+
 
 app.listen(3000, ()=>{
     console.log('server is successfully listaning on port 3000')
---
+++ src/notes.md
@@ -0,0 +1,28 @@
+create a repo
+npm inint
+install express
+create a server
+listen to port 3000/7777
+Write a request handlers 
+install nodemon and update the scripts inside package.json
+what are dependencies
+what is -g
+Diff (^ and ~)
+
+
+>>>>>>>>>>>>>>>>>>>>> Git creation <<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>
+
+
+initialize git (git init)
+.git ignore
+create a remote repo on git hub
+push all code to remote origin
+
+
+>>>>>>>>>>>>>>>>>>>>> Post man <<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>
+
+
+Install a postman 
+create a workspace
+create a colection 
+test Api call 
